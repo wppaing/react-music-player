8b6a9e1a925e550799c7f1055a82af8bf085176a
wppaing <wintphyopaing.p@gmail.com>
2022-02-02 11:27:40 +0630

    fixed a bug with rotate effect

--- src/components/Player.js
+++ src/components/Player.js
@@ -45,9 +45,9 @@ const Player = ({
       var values = matrix.split("(")[1].split(")")[0].split(",");
       var a = values[0];
       var b = values[1];
-      var angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+      let angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
     } else {
-      var angle = 0;
+      let angle = 0;
     }
     return angle;
   };
